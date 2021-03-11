d9cb52995ee31cc0094bf98e6d9b15fe6bc2ba53
rogeriodelphi <rogeriodelphi@gmail.com>
2021-03-11 13:15:28 -0300

    Criando os arquivos estáticos

---
+++ apps/moveis/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'apps.moveis'
+
+urlpatterns = [
+    # path('adicionar_usuario/', views.adicionar_usuario, name='adicionar_usuario'),
+]
